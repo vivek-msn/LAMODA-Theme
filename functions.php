462646a6b6b5a5b863545c7f820c370bbf6979c3
vivek-msn <vivek_msn@hotmail.com>
2024-04-28 01:38:04 +0530

    add_theme_supports functions declare in functions.php file

--- functions.php
+++ functions.php
@@ -9,7 +9,13 @@ function lamod_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'lamod_theme_scripts');
 
 function lamod_theme_setup(){
-
+    add_theme_support('custom-logo');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size( 'home-featured', 680,400,array('center','center'));
+    add_image_size( 'single-img', 600,550, array('center','center'));
+    add_theme_support('automatic-feed-links');
+    
     register_nav_menus( array(
         'primary' => __('Primary Menu', 'lamod') 
     ) );
