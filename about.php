af03cd07a51aba8d2c8eeda98303c831634b090f
vivek-msn <vivek_msn@hotmail.com>
2024-05-11 00:56:32 +0530

    make custom template of about us

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php 
+/** 
+ * Template Name: About
+ * 
+*/
+
+get_header(); ?>
+
+<!--Start Contact-->
+<section id="contact" class="section parallax">
+      <div class="overlay"></div>
+       <div class="container">
+           <div class="row">
+           
+                 <div class="title-box text-center white">
+                    <h2 class="title">About us</h2>
+                 </div>
+             </div>
+
+               <!--Start contact form-->
+               <div class="col-md-8 col-md-offset-2 contact-form">
+               
+                    <div class="contact-info text-center">
+                       <p>123 4567 890</p>
+                       <p>123 lorem ipsum, 4th floor, lorem, ipsum </p>
+                       <p>mail@demo.com </p>
+                    </div>                   
+               </div>
+              <!--End contact form-->
+              
+       </div> <!-- /.container-->
+   </section>
+   <!--End Contact-->
+
+<?php get_footer(); ?>
